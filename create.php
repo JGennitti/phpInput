592f24b1cbe109b0a7b3023f671bb331ed9ad2cc
Joey <info@josephgennitti.com>
2014-11-13 21:59:57 -0800

    added create php file

---
+++ create.php
@@ -0,0 +1,15 @@
+<?php 
+	include 'includes/connection.php';
+
+	$name = $_POST['inputName'];
+	$desc = $_POST['inputDesc'];
+
+	if(!$_POST['submit']) {
+		echo "Please fill out the form";
+		header('Location: index.php');
+	}else{
+		mysql_query("INSERT INTO people (`ID`, `Name`, `Description`) VALUES(NULL,'$name','$desc')") or die(mysql_error());
+		echo "User has been added!";
+		header('Location: index.php');
+	}
+?>
